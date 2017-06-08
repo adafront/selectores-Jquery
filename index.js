93e2a37832f2e6bc623f2ce3847f44c493648ea7
ada <adafront@gmail.com>
2017-06-08 12:36:42 -0300

    primera version

---
+++ index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+		//$('h1, h2').hide();
+		//$('p#primero').hide();
+		//$('p#primero a').hide();
+
+		/*$('#segunda').parent().css({
+			background: '#000'
+		});
+
+		$('#segunda').parents();*/
+
+		//$('#padre').children('#segunda').fadeOut(500);
+
+		//$('#segunda').siblings().fadeOut();
+
+		//$('#segunda').nextAll();
+		//$('#segunda').prev().css({
+		//	background: '#000'
+		//});
+
+		
+
+
+
+
+
+
+
+});
